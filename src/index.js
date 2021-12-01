f51e3b29e53cd72abc2fab36c2acc79baf9e8220
Gabriel Camboim <gabrielcamboim99@gmail.com>
2021-12-01 00:29:22 -0300

    NOVO ZAIVU/DROPDOWN SELECTION

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ const User = mongoose.model("User");
 
 const app = express();
 
+//comment
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
