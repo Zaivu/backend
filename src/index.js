e63f5f43915df06b605fd607fadd8e01f7fad775
Gabriel Camboim <gabrielcamboim99@gmail.com>
2023-08-28 09:36:54 -0300

    updated reactflow

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const app = express();
 //comment
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(
   "/files",
   express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
