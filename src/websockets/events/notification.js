d4688157a188ab65c9183725da488d4db89a887b
Brunogomes97 <brunogxy10@gmail.com>
2023-12-12 12:06:27 -0300

    notification micro adjustments

--- src/websockets/events/notification.js
+++ src/websockets/events/notification.js
@@ -1,6 +1,6 @@
 module.exports = function (socket) {
-    socket.on('notification', (data) => {
-        console.log({ data })
+    socket.on('notification', () => {
+        //console.log({ data })
 
         // socket.broadcast.emit('receive_notification', data)
         // lógica para lidar com notificações
